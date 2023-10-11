1fd27991cce08e6e89b26fc3814d1936f0d7c6ff
Pranav <119730521+22P31A4212@users.noreply.github.com>
2023-10-11 09:30:44 +0530

    n=int(input()) a=list(map(int,input().split())) s=sum(a) for i in range(n,-1,-1):     if(s%i==0):         print(i)         break else:     print(1)

--- Maximum_Identical_Bowls.py
+++ Maximum_Identical_Bowls.py
@@ -1,11 +1,9 @@
-def identical(a,b,c):
-    for i in range(c,0,-1):
-        if(b%i==0):
-            return i
-    else:
-        return 0
-
-c=int(input())
+n=int(input())
 a=list(map(int,input().split()))
-b=sum(a)
-print(identical(a,b,c))
+s=sum(a)
+for i in range(n,-1,-1):
+    if(s%i==0):
+        print(i)
+        break
+else:
+    print(1)
